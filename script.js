a1f04ce3b17c867c46b987c784daf6ac2ab97a98
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-05-03 15:54:33 -0400

    linked js, started btn functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Potential Music Solution https://stackoverflow.com/questions/13402336/play-sound-file-in-a-web-page-in-the-background
+
+const submitBtn = document.querySelector('.submit');
+const openHint = document.querySelector('.hint');
+
+openHint.addEventListener('click', presentHint);
+submitBtn.addEventListener('click', submitAnswer);
+
+
+function presentHint() {
+    console.log("This Hint is Working")
+}
+
+function submitAnswer() {
+    console.log("This Submit is Working")
+}
