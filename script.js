df42b9ba34b6c5cd18d9763c832b4bdfd272c036
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-05-03 17:37:50 -0400

    created set of question answers

--- script.js
+++ script.js
@@ -3,14 +3,25 @@
 const submitBtn = document.querySelector('.submit');
 const openHint = document.querySelector('.hint');
 
-openHint.addEventListener('click', presentHint);
 submitBtn.addEventListener('click', submitAnswer);
+openHint.addEventListener('click', presentHint);
 
 
+const answers1 = ['2001', '2002', '1999', '2004']
+const answers2 = ['Michiru Oshima', 'Hans Zimmer', 'Joe Hisaishi', 'Koji Kondo']
+const answers3 = ['Billy Crudup', 'Billy Bob Thornton', 'Kristen Dunst', 'Claire Danes']
+const answers4 = ['16 Months', '24 Months', '8 Months', '14 Months']
+const answers5 = ['My Neighbor Totoro', 'The Castle of Cogliostro', 'Spirited Away', 'Nausicaa of the Valley of the Wind']
+
+function submitAnswer() {
+    console.log("This Submit is Working")
+    for (i=0; i < questions.length; i++){
+
+    }
+}
 function presentHint() {
     console.log("This Hint is Working")
 }
 
-function submitAnswer() {
-    console.log("This Submit is Working")
-}
+// Seperate Arrays for Qustions and for Answers. When Submitted Answer is
+// equal to true anser (true), should move to next array option.
