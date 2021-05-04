c478b6e65c4eb29c34ab31fc42970a563f8bff08
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-05-04 10:53:24 -0400

    Replacing Question, not totally functional yet.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const openHint = document.querySelector('.hint');
 submitBtn.addEventListener('click', submitAnswer);
 openHint.addEventListener('click', presentHint);
 
-const question1 = ("What year did the film 'Sprited Away' relese in the United States?")
+const question1 = ("What year did the film 'Sprited Away' release in the United States?")
 const question2 = ("Who is the primary composer for Studio Ghibli's productions?")
 const question3 = ("Which of these actors was not part of the English Voice Cast for 'Princess Mononoke'?")
 const question4 = ("How long was 'My Neighbor Totoro' in production for?")
@@ -18,10 +18,23 @@ const answers3 = ['Billy Crudup', 'Billy Bob Thornton', 'Kristen Dunst', 'Claire
 const answers4 = ['16 Months', '24 Months', '8 Months', '14 Months']
 const answers5 = ['My Neighbor Totoro', 'The Castle of Cogliostro', 'Spirited Away', 'Nausicaa of the Valley of the Wind']
 
-function submitAnswer() {
-    console.log("This Submit is Working")
-    for (i=0; i < questions.length; i++){
+// If need to cheat, rather than an array for each question set, can use an array for each
+// question row. IE [2001, Michiru Oshima, Billy Crudup, etc.]
 
+const questionArray = [`${question1}`, `${question2}`, `${question3}`, `${question4}`, `${question5}`]
+const answerArray = [answers1, answers2, answers3, answers4, answers5]
+
+let currentQuestion = document.querySelector('.questions')
+
+// Every time the submit button is clicked, the function submitAnswer should review the questionArray, increase the array selected by one, and replace the question
+// with the new question. This increase of i should also reflect on the answers array, which should also increase by one and replace all li in their seperate fields. 
+
+function submitAnswer(event) {
+    event.preventDefault;
+    console.log("This Submit is Working");
+    for (let i=0; i < questionArray.length; i++){
+        console.log(i);
+        return currentQuestion.innerText = questionArray[i];
     }
 }
 function presentHint() {
